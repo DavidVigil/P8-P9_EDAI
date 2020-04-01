b5d2649027fdca11512c51fae127b764c0ef8ae9
DavidVigil <54321849+DavidVigil@users.noreply.github.com>
2020-03-31 21:01:08 -0600

    Add files via upload

---
+++ Calculadora_de_areas.py
@@ -0,0 +1,24 @@
+base_triangulo = 3
+altura_triangulo = 5
+base_rectangulo = 6
+altura_rectangulo = 4
+radio = 7
+base_mayor = 10
+base_menor = 5
+altura_trapecio = 8
+perimetro_triangulo = base_triangulo * 3
+area_triangulo = (base_triangulo * altura_triangulo) / 2
+perimetro_rectangulo = (base_rectangulo * 2) + (altura_rectangulo * 2)
+area_rectangulo = base_rectangulo * altura_rectangulo
+perimetro_circulo = radio * 3.14159
+area_circulo = 3.14159 * (radio * radio)
+perimetro_trapecio = ((base_mayor + base_menor)/2) + (altura_trapecio * 2)
+area_trapecio = ((base_mayor + base_menor) / 2) * altura_trapecio
+print("Perímetro del triángulo: " + str(perimetro_triangulo))
+print("Área del triángulo: " + str(area_triangulo))
+print("Perímetro del rectángulo: " + str(perimetro_rectangulo))
+print("Área del rectángulo: " + str(area_trapecio))
+print("Perímetro del círculo: " + str(perimetro_circulo))
+print("Área del círculo: " + str( area_circulo))
+print("Perímetro del trapecio: " + str(perimetro_trapecio))
+print("Área del trapecio: " + str(area_trapecio))
